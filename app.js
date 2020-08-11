c5340e47b14d3d93718472cc864d52ea61a7a6f4
bosshogg86 <jeffhogg86@gmail.com>
2020-08-10 23:52:43 -0700

    changed time display

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 $(document).ready(function () {
+    
+    const m = moment();
+    
+    // Display Date
     let date = moment().format('dddd, MMMM Do');
     $("#currentDay").text(date);
+
+    // Current time
+    let currentTime = m.hours();
+    console.log(currentTime);
+    // if (currentTime.toString === ) {
+
+    // }
+
 });
