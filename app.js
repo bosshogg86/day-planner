a1256f1d83aaee5bffc6a6d98d9589aa9483a076
bosshogg86 <jeffhogg86@gmail.com>
2020-08-10 22:35:12 -0700

    added current date

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 $(document).ready(function () {
-// const moment = require('moment');
-
-let now = moment();
-console.log(now.format());
-
-
+    
+    let date = moment().format('LLLL');
+    $("#currentDay").text(date);
+    
 
 });
