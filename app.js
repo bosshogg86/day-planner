17e192f432c5ab951ff59655aa85ed9d0f5e2432
bosshogg86 <jeffhogg86@gmail.com>
2020-08-11 21:31:07 -0700

    saves first input value

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
   const $textArea = $(".textarea");
-  console.log($textArea);
+  const $saveBtn = $(".saveBtn");
   const m = moment();
-
+  let userInput = $textArea.text();
+  let userInputs = [];
+  console.log(userInput);
   // Array of time-blocks
   const $timeBlock = $(".time-block");
-  console.log($timeBlock);
 
   // Display Date
   let date = moment().format("dddd, MMMM Do");
@@ -13,17 +14,29 @@ $(document).ready(function () {
 
   // Current time
   let currentTime = m.hours();
-  console.log(currentTime);
-
+ 
+  // Checks if time-block is past, present, future
   $textArea.each(function (i) {
     if (currentTime === i + 9) {
       $(this).addClass("present");
     } else if (currentTime > i + 9) {
-      console.log("past true");
       $(this).addClass("past");
     } else if (currentTime < i + 9) {
       $(this).addClass("future");
     }
-    console.log(currentTime);
   });
+
+  // 
+  $textArea.on("keyup change", function() {
+    $textArea.text(this.value);
+ });
+
+//   $saveBtn.on("click", function() {
+//     localStorage.setItem("userInputs", JSON.stringify($(".textarea")));
+//   });
+
+  $('button').on('click', function() {
+    localStorage.setItem("inputvalue", $textArea.val());
+    $textArea.text(localStorage.getItem("inputvalue"));
+});
 });
