2025e4f4369e448ae6e42eb4901cf5c51c9d4ec0
bosshogg86 <jeffhogg86@gmail.com>
2020-08-12 13:17:39 -0700

    seperate storage arrays

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
   const $textArea = $(".textarea");
-  // const $saveBtn = $(".saveBtn");
+  const $saveBtn = $(".saveBtn");
   const m = moment();
-//   let userInput = $textArea.text();
+
   let userInputs0 = [];
   let userInputs1 = [];
   let userInputs2 = [];
@@ -12,8 +12,6 @@ $(document).ready(function () {
   let userInputs6 = [];
   let userInputs7 = [];
   let userInputs8 = [];
-
-  // const $timeBlock = $(".time-block");
 
   // Display Date
   let date = moment().format("dddd, MMMM Do");
@@ -33,49 +31,50 @@ $(document).ready(function () {
     }
   });
 
-$("#btn0").on('click', function () {
+  // Stores text into array on btn click
+  $("#btn0").on('click', function () {
     userInputs0.push($('#input0').val());
     localStorage.setItem("item", JSON.stringify(userInputs0));
-});
+  });
 
-$("#btn1").on('click', function () {
-  userInputs1.push($('#input1').val());
-  localStorage.setItem("item", JSON.stringify(userInputs1));
-});
+  $("#btn1").on('click', function () {
+    userInputs1.push($('#input1').val());
+    localStorage.setItem("item", JSON.stringify(userInputs1));
+  });
 
-$("#btn2").on('click', function () {
-  userInputs2.push($('#input2').val());
-  localStorage.setItem("item", JSON.stringify(userInputs2));
-});
+  $("#btn2").on('click', function () {
+    userInputs2.push($('#input2').val());
+    localStorage.setItem("item", JSON.stringify(userInputs2));
+  });
 
-$("#btn3").on('click', function () {
-  userInputs3.push($('#input3').val());
-  localStorage.setItem("item", JSON.stringify(userInputs3));
-});
+  $("#btn3").on('click', function () {
+    userInputs3.push($('#input3').val());
+    localStorage.setItem("item", JSON.stringify(userInputs3));
+  });
 
-$("#btn4").on('click', function () {
-userInputs4.push($('#input4').val());
-localStorage.setItem("item", JSON.stringify(userInputs4));
-});
+  $("#btn4").on('click', function () {
+    userInputs4.push($('#input4').val());
+    localStorage.setItem("item", JSON.stringify(userInputs4));
+  });
 
-$("#btn5").on('click', function () {
-userInputs5.push($('#input5').val());
-localStorage.setItem("item", JSON.stringify(userInputs5));
-});
+  $("#btn5").on('click', function () {
+    userInputs5.push($('#input5').val());
+    localStorage.setItem("item", JSON.stringify(userInputs5));
+  });
 
-$("#btn6").on('click', function () {
-  userInputs6.push($('#input6').val());
-  localStorage.setItem("item", JSON.stringify(userInputs6));
-});
+  $("#btn6").on('click', function () {
+    userInputs6.push($('#input6').val());
+    localStorage.setItem("item", JSON.stringify(userInputs6));
+  });
 
-$("#btn7").on('click', function () {
-userInputs7.push($('#input7').val());
-localStorage.setItem("item", JSON.stringify(userInputs7));
-});
+  $("#btn7").on('click', function () {
+    userInputs7.push($('#input7').val());
+    localStorage.setItem("item", JSON.stringify(userInputs7));
+  });
 
-$("#btn8").on('click', function () {
-userInputs8.push($('#input8').val());
-localStorage.setItem("item", JSON.stringify(userInputs8));
-});
+  $("#btn8").on('click', function () {
+    userInputs8.push($('#input8').val());
+    localStorage.setItem("item", JSON.stringify(userInputs8));
+  });
 
 });
