b6b0b0079ac5265bf75077c08ab5badde6afa613
bosshogg86 <jeffhogg86@gmail.com>
2020-08-12 18:26:39 -0700

    refactor

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 $(document).ready(function () {
-  const $textArea = $(".textarea");
-  const $saveBtn = $(".saveBtn");
-  const m = moment();
-
-  // Storage array
+  
+  // Declare storage array
   let userInputs = [];
+
+  // Call moment
+  moment();
 
   // Display Date
   let date = moment().format("dddd, MMMM Do");
   $("#currentDay").text(date);
 
   // Grabs current time
-  let currentTime = m.hours();
-
+  let currentTime = moment().hours();
+  
   // Checks if time-block is past, present, future
-  $textArea.each(function (i) {
+  $(".textarea").each(function (i) {
     if (currentTime === i + 9) {
       $(this).addClass("present");
     } else if (currentTime > i + 9) {
@@ -26,7 +26,7 @@ $(document).ready(function () {
 
   // On btn click stores text input
   $(".time-block").each(function () {
-    $($saveBtn).on("click", function () {
+    $(".saveBtn").on("click", function () {
       userInputs.push($(this).siblings(".textarea").val());
       localStorage.setItem("input", JSON.stringify(userInputs));
     });
